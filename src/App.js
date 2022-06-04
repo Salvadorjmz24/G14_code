7888db4654539d71874cd759e9b2248cee242d69
Salvadorjmz24 <hector.salvador.lopez.jimenez@gmail.com>
2022-06-04 10:43:14 -0500

    do

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hola Mundo  estoy desde React!
+          Hola Mundo  estoy desde React!!!
+          hello word!!
         </p>
         <a
           className="App-link"
