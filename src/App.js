f1ca28724c57684788d0751958bc98f6caad9650
Salvadorjmz24 <hector.salvador.lopez.jimenez@gmail.com>
2022-06-04 09:47:34 -0500

    agregado un hola

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hola Mundo!
+          Hola Mundo  estoy desde React!
         </p>
         <a
           className="App-link"
