5e0eec059226fc4936c1c088a948e760396bec37
Salvadorjmz24 <hector.salvador.lopez.jimenez@gmail.com>
2022-07-20 09:39:17 -0500

    :bug: puse para que de compras regrese a home

--- Ecommers/02-ecommers/src/pages/Compras/Compras.jsx
+++ Ecommers/02-ecommers/src/pages/Compras/Compras.jsx
@@ -1,7 +1,13 @@
+import { useLocation, Link } from 'react-router-dom'
 const Compras = () => {
+  const location = useLocation()
   return (
 
-    <img className='image-detail' src='https://i.pinimg.com/564x/ec/41/bb/ec41bbdb0073809412f43f45c8f36b1d.jpg' alt='imagen producto' />
+    <>
+      <img className='image-detail' src='https://i.pinimg.com/564x/ec/41/bb/ec41bbdb0073809412f43f45c8f36b1d.jpg' alt='imagen producto' />
+      <p>Me aburrí de compras me iré a home :{location.pathname} </p>
+      <p><Link to='/'>Regresa al Home</Link></p>
+    </>
   )
 }
 
